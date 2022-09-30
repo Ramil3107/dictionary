65c0e4ebc6a86e7738357dbff8e504b0ec40b382
Ramil Abbasov <abbasov.ram3107@gmail.com>
2022-10-01 00:46:06 +0300

    add styles to practice page

--- src/pages/practice/components/Quiz.js
+++ src/pages/practice/components/Quiz.js
@@ -1,4 +1,5 @@
-import { Box, Button, Typography } from "@mui/material"
+import { Box, Button, Divider, Typography } from "@mui/material"
+import { blue, grey } from "@mui/material/colors"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import { setCounter, addCurrentGameRightAnswers, setRandomAnswers, setIsPracticeStarted } from "../../../redux/dictionarySlice"
@@ -15,7 +16,9 @@ export const Quiz = () => {
             mt: 15
         },
         buttons: {
-            display: "flex"
+            display: "flex",
+            flexWrap: "wrap",
+            mt: 5,
         }
     }
 
@@ -37,12 +40,25 @@ export const Quiz = () => {
 
     return (
         <Box sx={styles.wrapper}>
-            <Typography>{counter + 1} of 10 questions</Typography>
-            <Typography variant="h3">{randomWords[counter]?.en}</Typography>
+            <Typography color={grey[500]}>{counter + 1} of 10 questions</Typography>
+            <Divider
+                variant="middle"
+                orientation="horizontal"
+                sx={{ color: "grey", width: "40%", mt: 1, mb:1 }} />
+            <Typography variant="h2">{randomWords[counter]?.en}</Typography>
+            <Divider
+                variant="middle"
+                orientation="horizontal"
+                sx={{ color: "grey", width: "40%", mt: 1, mb: 3 }} />
+            <Box sx={{border:"0.5px grey solid"}}>
+                <Typography sx={{p:1}} color={grey[600]}>select the correct translate: </Typography>
+            </Box>
+
+
             <Box sx={styles.buttons}>
                 {
                     randomAnswers.map(answer => {
-                        return <Button key={answer.uk} onClick={() => handleAnswerButton(answer)} >{answer.uk}</Button>
+                        return <Button sx={{ fontSize: 20, m: 3 }} key={answer.uk} onClick={() => handleAnswerButton(answer)} >{answer.uk}</Button>
                     })
                 }
             </Box>
