09be4a538b7717bc247e446b2409442599d52365
Ramil Abbasov <abbasov.ram3107@gmail.com>
2022-10-02 00:56:00 +0300

    add close icon

--- src/pages/myDictionary/components/WordCard.js
+++ src/pages/myDictionary/components/WordCard.js
@@ -1,7 +1,9 @@
-import { Divider, Paper, Typography } from "@mui/material"
-import { blue, green } from "@mui/material/colors"
+import { Close, Delete } from "@mui/icons-material"
+import { Divider, IconButton, Paper, Typography } from "@mui/material"
+import { blue, green, grey } from "@mui/material/colors"
 
 export const WordCard = ({ word }) => {
+
     const styles = {
         wrapper: {
             width: 300,
@@ -21,12 +23,21 @@ export const WordCard = ({ word }) => {
         },
         translate: {
             color: green[300]
+        },
+        deleteIcon: {
+            alignSelf: "flex-end",
+            mr: 2,
+            mb: -7
         }
     }
+
     return (
         <Paper
             elevation={3}
             sx={styles.wrapper}>
+            <IconButton sx={{ alignSelf: "flex-end", mr: 2, mb: -7 }}>
+                <Close color="action" />
+            </IconButton>
             <Typography
                 sx={styles.word}
                 variant="h4" >
@@ -41,6 +52,7 @@ export const WordCard = ({ word }) => {
                 variant="h4">
                 {word.uk}
             </Typography>
+
         </Paper>
     )
 }
